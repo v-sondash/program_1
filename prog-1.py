fc390e63417c4d42483750c1aa992cc321791ade
v-sondash <v-sondash@microsoft.com>
2024-07-24 15:43:00 +0530

    My first commit

---
+++ prog-1.py
@@ -0,0 +1,61 @@
+"""1. A school has following rules for grading system:
+a. Below 25 - F
+b. 25 to 45 - E
+c. 45 to 50 - D
+d. 50 to 60 - C
+e. 60 to 80 - B
+f. Above 80 - A
+Ask user to enter marks and print the corresponding grade.
+"""
+# print("enter marks:")
+# marks = int(input())
+# if marks<25 :
+#     print("Fail: F")
+# elif marks>=25 and marks<45:
+#     print("E")
+# elif marks>=45 and marks<50:
+#     print("D")
+# elif marks>=50 and marks<60:
+#     print("C")
+# elif marks>=60 and marks<80:
+#     print("B")
+# elif marks>=80 and marks<100:
+#     print("A")
+# else:
+#     print("result withheld")
+
+"""2.Take input of age of 3 people by user and determine oldest and youngest among them."""
+# print("Enter the first age: ")
+# first_age = int(input()) #10
+# print("Enter the second age: ")
+# second_age = int(input()) #20
+# print("Enter the third age")
+# third_age = int(input()) #25
+# if first_age>=second_age and first_age>=third_age:
+#     print("first age is older...")
+# elif second_age>=first_age and second_age>=third_age:
+#     print("second age is older...")
+# elif third_age>=second_age and third_age>=first_age:
+#     print("third age is older")
+# else:
+#     print("first age is youngest...")
+
+#Swapping of elements in the list
+# str_lst = ["god", "is", "the", "only", "source", "to", "inner", "peace"]
+# print("the original list is :", str_lst)
+# first_index = str_lst.index("god")
+# second_index = str_lst.index("peace")
+# #Swap "god" with "peace" in the final list
+# str_lst[first_index] = "peace"
+# str_lst[second_index] = "god"
+# # result
+# print("result is :", str_lst)
+
+#Swapping of letters in the element of a list
+# str_lst = ["god", "is", "great", "give", "energy"]
+# print("the original list is :", str_lst)
+# # res = [sub.replace('g', 'T')for sub in str_lst]
+# res = str_lst[2].replace('g', 'T')
+# print("res is :", res)
+
+
